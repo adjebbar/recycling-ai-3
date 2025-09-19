ae63d94251139aaa38439d1200069de5ce3f7571
[dyad] <adjebbar83@gmail.com>
2025-09-19 15:28:01 +0100

    [dyad] Removed author line from footer. - wrote 3 file(s)

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -4,13 +4,10 @@ import { useTranslation } from "react-i18next";
 
 export const AppFooter = () => {
   const { t } = useTranslation();
-  const currentYear = new Date().getFullYear(); // This will still get the current year, but the user specified 2025 in the text. I'll use the hardcoded 2025 for the copyright line.
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="p-4 text-center text-sm text-gray-500 dark:text-gray-400">
-      <p className="mb-1">
-        {t('footer.author', 'Author')}: NewTech corp. (adjebbar@zohomail.com)
-      </p>
       <p className="mb-1">
         {t('footer.copyrightLine1', '© 2025 NewTech. All rights reserved.')}
       </p>
